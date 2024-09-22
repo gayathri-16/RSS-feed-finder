fb82e4053d6bc7411fc5b5e8752c4791436a11a3
Gayathri <gayathri.p973@gmail.com>
2024-09-22 22:44:52 +0530

    updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,24 +1,32 @@
 <template>
-  <div class="flex items-start flex-col w-full justify-start py-4">
-    <div class="flex items-center  justify-start py-4">
-      <label for="url" class="px-4 font-bold text-xl">Enter URL</label>
-      <input 
-        id="url"
-        v-model="url" 
-        @input="sendUrl" 
-        class="border border-gray-300 p-2 w-64 focus:outline-none rounded-lg" 
-        type="text" 
-        placeholder="Paste URL"
-      >
+  <div class="flex items-center justify-center h-screen"> <!-- Parent container -->
+    <div class="flex items-start px-4 h-1/2 flex-col w-1/2 justify-start py-4 shadow-custom">
+      <div class="flex items-center justify-start py-4">
+        <label for="url" class="px-4 font-bold text-xl">Enter URL</label>
+        <input 
+          id="url"
+          v-model="url" 
+          @input="sendUrl" 
+          class="border border-gray-300 p-2 w-64 focus:outline-none rounded-lg" 
+          type="text" 
+          placeholder="Paste URL"
+        >
+      </div>
+
+      <div class="flex px-4 items-start justify-start py-4">
+        <h4 class="font-bold text-xl">Your URL is categorized as</h4>
+        <p class="text-pink-700 font-bold text-xl pl-4">{{ category }}</p>
+      </div>
+
+      <div class="image-box"> <!-- Add overflow-hidden -->
+        <img 
+          v-if="thumbnail" 
+          :src="thumbnail" 
+          alt="Thumbnail" 
+          class=""
+        >
+      </div>
     </div>
-
-    <div class="flex items-center">
-      <h4 class="px-4 font-bold text-xl">Your URL is categorized as</h4>
-      <p class="text-pink-700 font-bold text-xl">{{ category }}</p>
-    </div>
-
-    <img v-if="thumbnail" :src="thumbnail" alt="Thumbnail" class="mt-4  px-4 w-1/3 h-full">
-
   </div>
 </template>
 
@@ -36,15 +44,16 @@ export default {
     async sendUrl() {
       if (!this.url) return; 
       try {
+        
         const response = await fetch('http://localhost:8000/api', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
           },
-          body: JSON.stringify({ url: this.url }), // Sending the URL in the request body
+          body: JSON.stringify({ url: this.url }),
         });
         
-        const data = await response.json(); // Assuming the response is JSON
+        const data = await response.json();
         this.category = data.category; 
         this.thumbnail = data.thumbnail; 
 
@@ -58,4 +67,17 @@ export default {
 
 <style scoped>
 /* Add any scoped styles here */
+.image-box{
+  width:400px;
+  height:300px;
+  margin-left:1rem;
+  object-fit: cover;
+position:relative;
+}
+.image-box > img{
+  max-width: 100%;
+  max-height: 100%;
+  position: absolute;
+  box-shadow: rgba(0, 0, 0, 0.05) 0px 1px 2px 0px;
+}
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow:{
+        'custom':'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;',
+        'img':'rgba(0, 0, 0, 0.05) 0px 1px 2px 0px'
+      }
+    },
   },
   plugins: [],
   purge: false
